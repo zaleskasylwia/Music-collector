428ae3b5e84b4eb5149a1e8eb615963adb2a2d4f
sylwia <zaleska.syl@gmail.com>
2017-04-16 19:59:27 +0200

    1st, basic version

---
+++ zad3.py
@@ -0,0 +1,40 @@
+
+music = [(("Pink Floyd", "The Dark Side Of The Moon"), (1973, "psychodelic rock", "43:00")),
+         (("Britney Spears", "Baby One More Time"), (1999, "pop", "42:20"))]
+
+def add_new_album():
+    music.append()
+
+def find_albums_by_musitions():
+
+def find_albums_by_year():
+
+def find_musician_by_album():
+
+
+def main():
+    print("Welcome in the CoolMusic! Choose the action:")
+    input_read = int(input())
+    if input_read == 1:
+        add_new_album()
+    elif input_read == 2:
+        find_albums_by_musitions()
+    elif input_read == 3:
+        add_new_album()
+    elif input_read == 4:
+        find_albums_by_musitions()
+    elif input_read == 5:
+        find_albums_by_musitions()
+    elif input_read == 6:
+        find_albums_by_musitions()
+    elif input_read == 7:
+        find_albums_by_musitions()
+    elif input_read == 8:
+        add_new_album()
+    elif input_read == 9:
+        find_albums_by_musitions()
+    elif input_read == 10:
+        find_albums_by_musitions()
+    elif input_read == 11:
+        find_albums_by_musitions()
+
